1a33ddd936d8c5cbfcd1acca36f6b5d03c6faa55
jeSuisCodeuse <creativegirl@outlook.fr>
2022-03-29 10:19:28 +0200

    Initial commit

---
+++ src/main/java/com/samia/chooseMyOutfit/ChooseMyOutfitApplication.java
@@ -0,0 +1,13 @@
+package com.samia.chooseMyOutfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseMyOutfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooseMyOutfitApplication.class, args);
+	}
+
+}

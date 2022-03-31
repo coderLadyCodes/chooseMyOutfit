7f70fe7108776c744f07bf9e83416d5000bea4a6
jeSuisCodeuse <creativegirl@outlook.fr>
2022-03-31 10:08:39 +0200

    entity set

--- src/main/java/com/samia/chooseMyOutfit/Outfits.java
+++ src/main/java/com/samia/chooseMyOutfit/Outfits.java
@@ -1,4 +1,42 @@
 package com.samia.chooseMyOutfit;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="outfits")
 public class Outfits {
+    public enum Thikness{
+        hot, normal, cold
+    }
+
+    public enum Cloths{
+        top, bottom, shoes
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "color", nullable = false)
+    private String color;
+
+    @Column(name = "comfy", nullable = false)
+    private Boolean comfy;
+
+    @Column(name = "chic", nullable = false)
+    private Boolean chic;
+
+    @Column(name = "thikness", nullable = false)
+    private Thikness thikness;
+
+    @Column(name = "cloths", nullable = false)
+    private Cloths cloths;
+
+
+    public Outfits() {
+    }
 }

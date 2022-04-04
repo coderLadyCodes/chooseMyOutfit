da9e9040a0f1762dcf48618f4b8f1eb6e5d74de9
jeSuisCodeuse <creativegirl@outlook.fr>
2022-04-04 21:47:34 +0200

    getters and setters

--- src/main/java/com/samia/chooseMyOutfit/Outfits.java
+++ src/main/java/com/samia/chooseMyOutfit/Outfits.java
@@ -59,4 +59,114 @@ public class Outfits {
 
     public Outfits() {
     }
+
+    public Outfits(String name, String color, String photoLink, boolean comfy, boolean chic, Thikness thikness, Cloths cloths, Shape shape) {
+        this.name = name;
+        this.color = color;
+        this.photoLink = photoLink;
+        this.comfy = comfy;
+        this.chic = chic;
+        this.thikness = thikness;
+        this.cloths = cloths;
+        this.shape = shape;
+    }
+
+    public Outfits(Long id, String name, String color, String photoLink, boolean comfy, boolean chic, Thikness thikness, Cloths cloths, Shape shape) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.photoLink = photoLink;
+        this.comfy = comfy;
+        this.chic = chic;
+        this.thikness = thikness;
+        this.cloths = cloths;
+        this.shape = shape;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPhotoLink() {
+        return photoLink;
+    }
+
+    public void setPhotoLink(String photoLink) {
+        this.photoLink = photoLink;
+    }
+
+    public boolean isComfy() {
+        return comfy;
+    }
+
+    public void setComfy(boolean comfy) {
+        this.comfy = comfy;
+    }
+
+    public boolean isChic() {
+        return chic;
+    }
+
+    public void setChic(boolean chic) {
+        this.chic = chic;
+    }
+
+    public Thikness getThikness() {
+        return thikness;
+    }
+
+    public void setThikness(Thikness thikness) {
+        this.thikness = thikness;
+    }
+
+    public Cloths getCloths() {
+        return cloths;
+    }
+
+    public void setCloths(Cloths cloths) {
+        this.cloths = cloths;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+
+    @Override
+    public String toString() {
+        return "Outfits{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", photoLink='" + photoLink + '\'' +
+                ", comfy=" + comfy +
+                ", chic=" + chic +
+                ", thikness=" + thikness +
+                ", cloths=" + cloths +
+                ", shape=" + shape +
+                '}';
+    }
 }

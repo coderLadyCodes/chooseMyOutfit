3ca802611c3c80611fa716445678d018e0e893b6
jeSuisCodeuse <creativegirl@outlook.fr>
2022-04-09 06:58:32 +0200

    entity

--- src/main/java/com/samia/chooseMyOutfit/Outfit.java
+++ src/main/java/com/samia/chooseMyOutfit/Outfit.java
@@ -32,7 +32,7 @@ public class Outfit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
 
     @Column(name = "color", nullable = false)
@@ -57,116 +57,11 @@ public class Outfit {
     @Column(name = "shape", nullable = false)
     private Shape shape;
 
+    @Column(name = "content", nullable = false)
+    private byte[] content;
+
     public Outfit() {
     }
 
-    public Outfit(String name, String color, String photoLink, boolean comfy, boolean chic, Thikness thikness, Cloths cloths, Shape shape) {
-        this.name = name;
-        this.color = color;
-        this.photoLink = photoLink;
-        this.comfy = comfy;
-        this.chic = chic;
-        this.thikness = thikness;
-        this.cloths = cloths;
-        this.shape = shape;
-    }
 
-    public Outfit(Long id, String name, String color, String photoLink, boolean comfy, boolean chic, Thikness thikness, Cloths cloths, Shape shape) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.photoLink = photoLink;
-        this.comfy = comfy;
-        this.chic = chic;
-        this.thikness = thikness;
-        this.cloths = cloths;
-        this.shape = shape;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getPhotoLink() {
-        return photoLink;
-    }
-
-    public void setPhotoLink(String photoLink) {
-        this.photoLink = photoLink;
-    }
-
-    public boolean isComfy() {
-        return comfy;
-    }
-
-    public void setComfy(boolean comfy) {
-        this.comfy = comfy;
-    }
-
-    public boolean isChic() {
-        return chic;
-    }
-
-    public void setChic(boolean chic) {
-        this.chic = chic;
-    }
-
-    public Thikness getThikness() {
-        return thikness;
-    }
-
-    public void setThikness(Thikness thikness) {
-        this.thikness = thikness;
-    }
-
-    public Cloths getCloths() {
-        return cloths;
-    }
-
-    public void setCloths(Cloths cloths) {
-        this.cloths = cloths;
-    }
-
-    public Shape getShape() {
-        return shape;
-    }
-
-    public void setShape(Shape shape) {
-        this.shape = shape;
-    }
-
-    @Override
-    public String toString() {
-        return "Outfits{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", color='" + color + '\'' +
-                ", photoLink='" + photoLink + '\'' +
-                ", comfy=" + comfy +
-                ", chic=" + chic +
-                ", thikness=" + thikness +
-                ", cloths=" + cloths +
-                ", shape=" + shape +
-                '}';
-    }
 }

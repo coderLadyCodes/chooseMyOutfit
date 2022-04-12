ac46f387b5d2955594c2f0df50a4c6f4405d9281
jeSuisCodeuse <creativegirl@outlook.fr>
2022-04-12 22:47:37 +0200

    datasql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO outfits (name, color, photo_link, comfy, chic, thikness, cloths, shape, content) VALUES ('t-shirt', 'rose', 'jdkjgikzejgi.jpg', true, false, 1, 1,2,10);
+
+INSERT INTO outfits (name, color, photo_link, comfy, chic, thikness, cloths, shape, content) VALUES ('top', 'green', 'jdkjgik.jpg', true, true, 1, 1,1,11);
